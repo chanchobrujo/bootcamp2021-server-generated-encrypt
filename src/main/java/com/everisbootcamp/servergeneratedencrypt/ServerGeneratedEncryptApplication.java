358029675719ad0fedc607c59668001540b7d02e
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 23:24:46 -0500

    Update 1

---
+++ src/main/java/com/everisbootcamp/servergeneratedencrypt/ServerGeneratedEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.servergeneratedencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGeneratedEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGeneratedEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everisbootcamp/servergeneratedencrypt/ServerGeneratedEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.servergeneratedencrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGeneratedEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
