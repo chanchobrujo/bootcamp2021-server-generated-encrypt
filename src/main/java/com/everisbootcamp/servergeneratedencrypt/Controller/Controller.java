3029d4c58369830e46624a519a8fd8cbe6b70030
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-18 18:23:52 -0500

    Update good

--- src/main/java/com/everisbootcamp/servergeneratedencrypt/Controller/Controller.java
+++ src/main/java/com/everisbootcamp/servergeneratedencrypt/Controller/Controller.java
@@ -1,37 +1,34 @@
 package com.everisbootcamp.servergeneratedencrypt.Controller;
 
+import com.everisbootcamp.servergeneratedencrypt.service.ServiceLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.everisbootcamp.servergeneratedencrypt.service.ServiceLogic;
-
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping
 public class Controller {
-	@Autowired
-	private ServiceLogic service;
-	
+
+    @Autowired
+    private ServiceLogic service;
+
     @GetMapping("/generatednumber/{size}")
-    public Mono< ResponseEntity<String> > generatedNumber(@PathVariable("size") int size){
+    public Mono<ResponseEntity<String>> generatedNumber(@PathVariable("size") int size) {
         return service
-        		.generatedNumber(size)
-        		.map(s-> ResponseEntity.ok().body(s) )
-        		.defaultIfEmpty(ResponseEntity
-        		.internalServerError().build());
+            .generatedNumber(size)
+            .map(s -> ResponseEntity.ok().body(s))
+            .defaultIfEmpty(ResponseEntity.internalServerError().build());
     }
-	
+
     @GetMapping("/encriptpassword/{password}")
-    public Mono< ResponseEntity<String> > encriptPassword(@PathVariable("password") String password){
+    public Mono<ResponseEntity<String>> encriptPassword(@PathVariable("password") String password) {
         return service
-        		.encriptPassword(password)
-        		.map(s-> ResponseEntity.ok().body(s) )
-        		.defaultIfEmpty(ResponseEntity
-        		.internalServerError().build());
+            .encriptPassword(password)
+            .map(s -> ResponseEntity.ok().body(s))
+            .defaultIfEmpty(ResponseEntity.internalServerError().build());
     }
 }
--- src/main/java/com/everisbootcamp/servergeneratedencrypt/ServerGeneratedEncryptApplication.java
+++ src/main/java/com/everisbootcamp/servergeneratedencrypt/ServerGeneratedEncryptApplication.java
@@ -1,19 +1,17 @@
 package com.everisbootcamp.servergeneratedencrypt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class ServerGeneratedEncryptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerGeneratedEncryptApplication.class, args);
-		log.info("SERVER LOGIC ENABLED"); 
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGeneratedEncryptApplication.class, args);
+        log.info("SERVER LOGIC ENABLED");
+    }
 }
